f6dac5a69470d12c04626f1eca714ecc31452a33
Arohan Sharma <arohansharma@Arohans-MacBook-Pro.local>
2024-10-18 18:57:12 -0500

    PCA Implementation

---
+++ factor_analysis/factorDecomposition.m
@@ -0,0 +1,30 @@
+function [estFactorRtns, portBetas, factorVols] = factorDecomposition(mktRtns, myPositions, params)
+   
+    % Computing the covariance matrix
+    factorCovMat = cov(mktRtns);
+    
+    % Check Model Type
+    if params.modelType == "PCA"
+        % Apply PCA to compute Factor Loadings
+        factorLoadings = pca(factorCovMat);
+        % Consider only the first k principal components
+        k = params.nFactorsToCompute;
+        factorLoadings = factorLoadings(:, 1:k);
+        
+        % Compute Factor returns, Portfolio Betas, and Factor Vols
+        estFactorRtns = mktRtns*factorLoadings;
+        portBetas = myPositions*factorLoadings;
+        factorVols = std(estFactorRtns);
+
+    elseif params.modelType == "PAF"
+        ME = MException('Model Type %s is not implemented', ...
+            params.modelType);
+        throw(ME);
+
+    else
+        ME = MException('Model Type %s is not implemented', ...
+            params.modelType);
+        throw(ME);
+
+    end
+end
